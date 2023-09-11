ca52f9625db05969c50c5f0dddebaf3c75467e3b
lbailey58 <lbailey58@charlottesvilleschools.org>
2023-09-11 15:54:21 -0400

    update

--- distanceSensor.py
+++ distanceSensor.py
@@ -2,12 +2,22 @@ import time
 import board
 import adafruit_hcsr04
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D7, echo_pin=board.D8)
-
+red = 0
+green = 0
 
 
 while True:
     try:
-        print((sonar.distance,))
+        if(sonar.distance < 20):
+            red = 255
+            green = (sonar.distance-20)*-12.75
+        elif(sonar.distance is 20):
+            red = 255
+            green = 255
+        else:
+            green = 255
+            red = (sonar.distance-20)*12.75
     except RuntimeError:
         print("Retrying!")
+    
     time.sleep(0.1)
