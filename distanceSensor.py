236d9de3784d14063193ed5d5aab0c0981745153
lbailey58 <lbailey58@charlottesvilleschools.org>
2023-09-13 15:53:42 -0400

    worked more

--- distanceSensor.py
+++ distanceSensor.py
@@ -3,27 +3,37 @@ import board
 import adafruit_hcsr04
 import pwmio
 
-
+cm = 0
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D7, echo_pin=board.D8)
-red = pwmio.PWMOut(board.D10)
-green = pwmio.PWMOut(board.D11)
-blue = pwmio.PWMOut(board.D12)
+red = pwmio.PWMOut(board.D10, frequency=5000, duty_cycle=0)
+green = pwmio.PWMOut(board.D11, frequency=5000, duty_cycle=0)
+blue = pwmio.PWMOut(board.D12, frequency=5000, duty_cycle=0)
 
 while True:
     try:
-        if(sonar.distance < 20):
+        cm = sonar.distance
+        if(cm < 20 and cm > 5):
             red.duty_cycle = 65535   
-            green.duty_cycle = (sonar.distance-20)*-3276.75
+            green.duty_cycle = int((cm-5)*4368)
             blue.duty_cycle = 0 
-        elif(sonar.distance is 20):
+        elif(cm is 20):
             red.duty_cycle = 65535
             green.duty_cycle = 65535
             blue.duty_cycle = 0
-        else:
+        elif(cm > 20 and cm < 35):
             green.duty_cycle = 65535
-            red.duty_cycle = (sonar.distance-20)*3276.75
+            red.duty_cycle = int((cm-20)*4368)
+            blue.duty_cycle = 0
+        elif(cm < 5):
+            red.duty_cycle = 65535
+            green.duty_cycle = 0
+            blue.duty_cycle = 0
+        else:
+            red.duty_cycle = 0
+            green.duty_cycle = 65535
             blue.duty_cycle = 0
     except RuntimeError:
         print("Retrying!")
-    
+    print("red: " + str(red.duty_cycle), "green: " + str(green.duty_cycle), "blue: " + str(blue.duty_cycle), )
+    print(cm)
     time.sleep(0.1)
